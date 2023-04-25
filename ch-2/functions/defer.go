3ee1545e9d2aaf4a77879bcd8d47558dddcda694
Nemuel Wainaina <bR0S3@protonmail.com>
2023-04-25 11:52:28 +0300

    defer keyword demo

---
+++ ch-2/functions/defer.go
@@ -0,0 +1,33 @@
+// understanding the defer keyword using a demo
+package main
+
+import (
+	"fmt"
+)
+
+func a1() {
+	for i:=0; i<3; i++ {
+		defer fmt.Print(i, " ")
+	}
+}
+
+func a2() {
+	for i:=0; i<3; i++ {
+		defer func() { fmt.Print(i, " ") } ()
+	}
+}
+
+func a3() {
+	for i:=0; i<3; i++ {
+		defer func(n int) { fmt.Print(i, " ") } (i)
+	}
+}
+
+func main() {
+	a1()
+	fmt.Println()
+	a2()
+	fmt.Println()
+	a3()
+	fmt.Println()
+}
