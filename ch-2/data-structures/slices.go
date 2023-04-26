2dd09f1bb460419245ba2cd68380f248bdaa7877
Nemuel Wainaina <bR0S3@protonmail.com>
2023-04-26 10:20:23 +0300

    More on slices

--- ch-2/data-structures/slices.go
+++ ch-2/data-structures/slices.go
@@ -23,4 +23,12 @@ func main() {
 	change(mySlice)
 	fmt.Println("After change :")
 	printSlice(mySlice)
+
+	fmt.Printf("Before : cap:%d len:%d\n", cap(mySlice), len(mySlice))
+	mySlice = append(mySlice, 100)
+	fmt.Printf("After : cap:%d len:%d\n", cap(mySlice), len(mySlice))
+
+	another := make([]int, 4)
+	fmt.Println("New slice with 4 elements")
+	printSlice(another)
 }
