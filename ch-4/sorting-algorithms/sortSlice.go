a17ec90c7cd879328ad830d0e2c3d325b573fee4
Nemuel Wainaina <bR0S3@protonmail.com>
2023-05-03 10:17:44 +0300

    Sorting in Go using sort.Slice()

---
+++ ch-4/sorting-algorithms/sortSlice.go
@@ -0,0 +1,35 @@
+// sort.Slice() function
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type aStructure struct {
+	person string
+	weight int
+	height int
+}
+
+func main() {
+	mySlice := make([]aStructure, 0)
+	a := aStructure{"Mihalis", 180, 90}
+	mySlice = append(mySlice, a)
+	a = aStructure{"Dimitris", 180, 95}
+	mySlice = append(mySlice, a)
+	a = aStructure{"Marietta", 155, 45}
+	mySlice = append(mySlice, a)
+	a = aStructure{"Bill", 134, 40}
+	mySlice = append(mySlice, a)
+
+	fmt.Println("0 :", mySlice)
+	sort.Slice(mySlice, func(i, j int) bool {
+		return mySlice[i].weight < mySlice[j].weight
+	})
+	fmt.Println("< :", mySlice)
+	sort.Slice(mySlice, func(i, j int) bool {
+		return mySlice[i].weight > mySlice[j].weight
+	})
+	fmt.Println("> :", mySlice)
+}
